3bc6856d15a8f77f015930d5c199f56e2e476d14
saahilkumar <60633758+saahilkumar@users.noreply.github.com>
2021-12-06 01:53:21 -0500

    login page frontend

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button, Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
 
 const LoginPage = () => {
-  return <div>login</div>;
+  return <div>
+    <Box sx={{width: "50%"}}>
+      <TextField fullWidth label="Username" id="username-field" />
+      <TextField fullWidth label="Password" id="fullWidth" sx={{mt: 1.5}}/>
+      <Button variant="contained" sx={{width: "100%", mt: 1.5, backgroundColor: "rgb(33, 112, 33)"}}>Login</Button>
+      <Button variant="contained" sx={{width: "100%", mt: 1.5, backgroundColor: "rgb(150, 150, 150)"}}>Sign Up</Button>
+    </Box>
+  </div>;
 };
 
 export default LoginPage;
