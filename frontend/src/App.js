6ee7b613674783b1d5117670615680287b01967f
saahilkumar <60633758+saahilkumar@users.noreply.github.com>
2021-12-06 17:59:09 -0500

    changing route for register page and adding radio button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ const Body = ({ drawerWidth }) => {
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/details" exact element={<DetailsPage />}></Route>
           <Route path="/login" exact element={<LoginPage />}></Route>
-          <Route path="/signup" exact element={<SignupPage />}></Route>
+          <Route path="/register" exact element={<SignupPage />}></Route>
           <Route path="/profile" exact element={<ProfilePage />}></Route>
           <Route path="/search" exact element={<SearchPage />}></Route>
           <Route path="/*" element={<Homepage />}></Route>
