e808e2e54cc9090ac1a05bb01ce8e5d853ba72dd
yanDavid21 <yanDavid21@users.noreply.github.com>
2021-12-07 15:54:09 -0500

    added isbn functionality

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -8,11 +8,12 @@ const APIKEY = `:keyes&key=AIzaSyCW29m4Tkp1NtCdM2jWzvqP9n-gjyO7Wl0`;
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   const query = req.query.q;
+  const isbn = req.query.isbn
   const title = req.query.title;
   const subject = req.query.subject;
   const publisher = req.query.publisher;
   const author = req.query.author;
-  const url = `${searchAPI}?q=${query}${title && `+intitle:${title}`}${subject && `+subject:${subject}`}${publisher && `+inpublisher:${publisher}`}${author && `+inauthor:${author}`}${APIKEY}` 
+  const url = `${searchAPI}?q=${query}${title && `+intitle:${title}`}${subject && `+subject:${subject}`}${publisher && `+inpublisher:${publisher}`}${author && `+inauthor:${author}`}${isbn && `+isbn:${isbn}`}${APIKEY}` 
   axios({
     method: "get",
     url: url,
