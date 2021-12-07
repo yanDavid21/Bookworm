ea355b78c988d4a968db76f9bc710daf721f71f3
yanDavid21 <yanDavid21@users.noreply.github.com>
2021-12-07 15:51:16 -0500

    implemented search route

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -8,13 +8,17 @@ const APIKEY = `:keyes&key=AIzaSyCW29m4Tkp1NtCdM2jWzvqP9n-gjyO7Wl0`;
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   const query = req.query.q;
-  const searchType = req.query.searchType;
+  const title = req.query.title;
+  const subject = req.query.subject;
+  const publisher = req.query.publisher;
+  const author = req.query.author;
+  const url = `${searchAPI}?q=${query}${title && `+intitle:${title}`}${subject && `+subject:${subject}`}${publisher && `+inpublisher:${publisher}`}${author && `+inauthor:${author}`}${APIKEY}` 
   axios({
     method: "get",
-    url: `${searchAPI}?q=inauthor${APIKEY}`,
+    url: url,
   }).then((response) => {
     console.log(response.data);
-    res.json(response.data);
+    res.json(response.date);
   });
 });
 
