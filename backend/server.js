77b50563012754382435e4c317239b395d4b5335
yanDavid21 <yanDavid21@users.noreply.github.com>
2021-12-14 18:44:21 -0500

    routing debug

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const createError = require("http-errors");
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -12,75 +13,78 @@ let db_uri = process.env.MONGODB_URI;
 
 // For local testing, for now
 if (!db_uri) {
-  db_uri = "mongodb+srv://deanframe:mongodbpass@web-dev-final-jose.vcajy.mongodb.net/final_project?retryWrites=true&w=majority"
+  db_uri =
+    "mongodb+srv://deanframe:mongodbpass@web-dev-final-jose.vcajy.mongodb.net/final_project?retryWrites=true&w=majority";
 }
 
 /***********middleware ************/
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, "build")));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 /************************** */
-
 
 /*************** database *****************/
 async function connectToDatabase() {
-  await mongoose.connect(db_uri)
+  await mongoose.connect(db_uri);
 }
-connectToDatabase().catch(err => console.log(err))
+connectToDatabase().catch((err) => console.log(err));
 /*****************************************/
 
 /*************** routes *******************/
-const indexRouter = require('./routes/index');
-const searchRouter = require('./routes/search');
-const createUserRouter = require('./routes/create-user');
-const getCurrentUserDataRouter = require('./routes/get-current-user')
-const getOtherUserDataRouter = require('./routes/get-other-user')
-const loginRouter = require('./routes/login');
-const logoutRouter = require('./routes/logout');
-const detailRouter = require('./routes/details');
-const bookRouter = require('./routes/book');
-const changeUserInfoRouter = require('./routes/change-user-info');
-const enterPasswordRouter = require('./routes/enter-password');
-const getPopularBooksRouter = require('./routes/get-popular-books');
-const getReadingListRouter = require('./routes/get-reading-list');
-const getBooksByAuthorRouter = require('./routes/get-books-by-author');
+const searchRouter = require("./routes/search");
+const createUserRouter = require("./routes/create-user");
+const getCurrentUserDataRouter = require("./routes/get-current-user");
+const getOtherUserDataRouter = require("./routes/get-other-user");
+const loginRouter = require("./routes/login");
+const logoutRouter = require("./routes/logout");
+const detailRouter = require("./routes/details");
+const bookRouter = require("./routes/book");
+const changeUserInfoRouter = require("./routes/change-user-info");
+const enterPasswordRouter = require("./routes/enter-password");
+const getPopularBooksRouter = require("./routes/get-popular-books");
+const getReadingListRouter = require("./routes/get-reading-list");
+const getBooksByAuthorRouter = require("./routes/get-books-by-author");
 
-app.use('/api/search', searchRouter);
+app.use("/api/search", searchRouter);
 app.use(cors());
-app.use('/api/register', createUserRouter);
-app.use('/api/get-current-user-data', getCurrentUserDataRouter);
-app.use('/api/get-other-user-data', getOtherUserDataRouter);
-app.use('/api/login', loginRouter);
-app.use('/api/logout', logoutRouter)
-app.use('/api/details', detailRouter);
-app.use('/api/book', bookRouter)
-app.use('/api/change-user-info', changeUserInfoRouter);
-app.use('/api/enter-password', enterPasswordRouter);
-app.use('/api/get-popular-books', getPopularBooksRouter);
-app.use('/api/get-reading-list', getReadingListRouter)
-app.use('/api/get-books-by-author', getBooksByAuthorRouter)
-app.use('/', indexRouter);
+app.use("/api/register", createUserRouter);
+app.use("/api/get-current-user-data", getCurrentUserDataRouter);
+app.use("/api/get-other-user-data", getOtherUserDataRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/logout", logoutRouter);
+app.use("/api/details", detailRouter);
+app.use("/api/book", bookRouter);
+app.use("/api/change-user-info", changeUserInfoRouter);
+app.use("/api/enter-password", enterPasswordRouter);
+app.use("/api/get-popular-books", getPopularBooksRouter);
+app.use("/api/get-reading-list", getReadingListRouter);
+app.use("/api/get-books-by-author", getBooksByAuthorRouter);
+router.get(["/", "/*"], function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+app.get("*", (req, res) => {
+  res.render("index", { title: "Express" });
+});
 /*******************************************/
-
 
 /**************** error handler ******************/
 //catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 //error handler func
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 /***********************************************/
 
