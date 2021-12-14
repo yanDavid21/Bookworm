85d95eef2f5021c4113235c4e060c758dab0d190
Dean Frame <dacframe@gmail.com>
2021-12-13 20:24:10 -0500

    remove some prints

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -13,12 +13,10 @@ router.post('/', function (req, res) {
     if (validated) {
       freeUserDao.findUserByEmail(email).then(user => {
         const userId = user.id
-        console.log(userId)
         // success!!
         // generate a new cookie
         const hashString = email + password + Date.now()
         bcrypt.hash(hashString, 10, function (err, hash) {
-          console.log(hash)
           cookieDao.createOrUpdateCookie(userId, hash, function (promise) {
             promise.then(cookie => {
               console.log("UPDATED COOKIE: " + cookie)
@@ -36,17 +34,17 @@ router.post('/', function (req, res) {
         if (validated) {
           paidUserDao.findUserByEmail(email).then(user => {
             const userId = user.id
-            console.log()
-            console.log(userId)
             // success!!
             // generate a new cookie
             const hashString = email + password + Date.now()
             bcrypt.hash(hashString, 10, function (err, hash) {
-              console.log(hash)
-              cookieDao.createOrUpdateCookie(userId, hash).then(cookie => {
-                res.status(200).send({
-                  message: "Login successful.",
-                  token: cookie.cookie,
+              cookieDao.createOrUpdateCookie(userId, hash, function (promise) {
+                promise.then(cookie => {
+                  console.log("UPDATED COOKIE: " + cookie)
+                  res.status(200).send({
+                    message: "Login successful.",
+                    token: cookie.cookie,
+                  })
                 })
               })
             })
